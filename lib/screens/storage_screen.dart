1a70f2b95126e575d78f9b214619a937acfdaa37
Douglas Nhunzvi <dougiedj@gmail.com>
2024-05-02 17:42:58 +0200

    upload options

--- lib/screens/storage_screen.dart
+++ lib/screens/storage_screen.dart
@@ -4,15 +4,22 @@ import 'package:gdrive_clone/widgets/storage_container.dart';
 import '../widgets/upload_options.dart';
 
 class StorageScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 40,),
-        StorageContainer(),
-        UploadOptions()
-      ],
+    return Expanded(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          StorageContainer(),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 18.0),
+            child: UploadOptions(),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/upload_options.dart
+++ lib/widgets/upload_options.dart
@@ -1,9 +1,52 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class UploadOptions extends StatelessWidget {
 
+  Widget coloredContainer(Color bgColor, Icon icon, String options, String title) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: bgColor,
+      ),
+      child: Center(
+        child: icon,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        coloredContainer(
+          Colors.lightBlue.withOpacity(0.2),
+          const Icon(Icons.image, color: Colors.cyan, size: 30,),
+          "image",
+          "Images"
+        ),
+        coloredContainer(
+            Colors.pink.withOpacity(0.3),
+            Icon(Icons.play_arrow_rounded, color: Colors.pink.withOpacity(0.5), size: 42,),
+            "video",
+            "Videos"
+        ),
+        coloredContainer(
+            Colors.blue.withOpacity(0.4),
+            Icon(EvaIcons.fileText, color: Colors.indigoAccent.withOpacity(0.4), size: 30,),
+            "application",
+            "Documents"
+        ),
+        coloredContainer(
+            Colors.purple.withOpacity(0.2),
+            Icon(EvaIcons.music, color: Colors.pink.withOpacity(0.3), size: 30,),
+            "images",
+            "Images"
+        ),
+      ],
+    );
   }
 }
